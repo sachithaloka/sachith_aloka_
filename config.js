5aa3740071dc5f65ec38f796161f95a0c88c8c5a
Aloka Perera <176225978+sachithaloka@users.noreply.github.com>
2025-02-12 06:55:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "CEBHlIxL#zzWdwOwRlUIwrtDxqiKKv-n3wGb-mULuvt2Zd-Au7MY", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true", //true or false 
